d303a422191482f346d38af7ae3432eaeb6b15c9
Vyshnav NP <84016314+vyshnavnp@users.noreply.github.com>
2025-07-30 04:30:57 +0530

    Add files via upload

---
+++ streamlitapp.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from preprocess import prepare_df
+
+model = joblib.load("logisticregression.pkl")
+
+st.title("Movie Review Sentiment Analysis")
+
+st.write("Enter your review for the movie")
+user_input = st.text_area("How do you feel about this movie?",height=150)
+
+if st.button("Predict Sentiment"):
+    if user_input.strip() == "":
+        st.warning("Please Enter a Review")
+    else:
+        df = pd.DataFrame({"review":[user_input]})
+        processed_df = prepare_df(df)
+
+        prediction = model.predict(processed_df)[0]
+        label = "Positive 😀" if prediction == "positive" else "Negative 😞"
+
+        st.subheader("Prediction:")
+        st.success(f"This Review is *{label}*")
